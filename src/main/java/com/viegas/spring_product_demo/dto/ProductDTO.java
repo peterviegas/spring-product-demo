12ccd09452b859d32f15a94f119953ebde8bbfbb
Peter Viegas <peterviegas@outlook.com>
2025-07-21 07:45:30 +0100

    feat(Product): created DTO

--- src/main/java/com/viegas/spring_product_demo/dto/ProductDTO.java
+++ src/main/java/com/viegas/spring_product_demo/dto/ProductDTO.java
@@ -1,4 +1,18 @@
 package com.viegas.spring_product_demo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private boolean active;
 }
